9368d122b1b447bab675724c3721b8266f222b70
gu-facilities <jr2151@georgetown.edu>
2026-03-15 22:41:06 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 //  STEP 3 of setup: paste your Apps Script Web App URL below.
 //  This file is loaded by index.html automatically.
 // ================================================================
-window.SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbx4Ec5wLxBpz-bzMofigL0JiiFh4T0zgwRZ3EvkaBN6z9dOggRX0cmOno8DfbNCaDneBg/exec';
+window.SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbyfrR6aI3_a1BGM1XEWMpWzBI4Di1iXULlI3zB2jYSckbcdGnXswLXQP72_XPNuXQhGaA/exec';
 // Example: window.SHEET_API_URL = https://script.google.com/macros/s/AKfycbyMpdxnc8KH18lZlYK8lzniKxP71ulomkPZ2J1tMeLLzGTDRY0au1q-3_EW25ubkMhH/exec
